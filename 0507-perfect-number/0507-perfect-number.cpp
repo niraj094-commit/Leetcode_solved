96702b915bb88d2275bb02eb70ecad1c0817496f
Niraj Kumar Yadav <n799612@gmail.com>
2026-07-22 12:09:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (56.69%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0) sum+=i;
+        int sum=1;
+        if(num<=1) return false;
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                sum+=i;
+
+                if(i*i!=num){
+                    sum+=num/i;
+                }
+            }
         }
 
         return sum==num;
